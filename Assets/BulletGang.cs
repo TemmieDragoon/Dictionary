338d0ee901c5c9ccb9f2ff8a8b60e0271672535d
Temmie <25546@ma-web.nl>
2020-03-27 14:55:34 +0100

    Initial commit

---
+++ Assets/BulletGang.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletGang : MonoBehaviour
+{
+    private enum Weapons
+    {
+        USP,
+        SPAS,
+        GLOCK
+    }
+
+    private readonly Dictionary<Weapons, int> _Weapons = new Dictionary<Weapons, int>();
+    private Weapons currentWeapon;
+
+    private void Start()
+    {
+        _Weapons.Add(0, 7);
+        //currentWeapon = Weapons.USP;
+    }
+
+    private void Update()
+    {
+
+        if (_Weapons.ContainsKey(currentWeapon))
+        {
+            if (Input.GetKeyDown("a"))
+            {
+                currentWeapon = Weapons.USP;
+                Debug.Log("USP");
+
+            }
+
+            if (Input.GetKeyDown("s"))
+            {
+                currentWeapon = Weapons.SPAS;
+                Debug.Log("SPAS");
+
+            }
+
+            if (Input.GetKeyDown("d"))
+            {
+                currentWeapon = Weapons.GLOCK;
+                Debug.Log("GLOCK");
+
+            }
+        }
+    
+    
+    
+    }
+
+}
